06c4f5b98d8c346627663a5689b426695915f631
Ashish <ashishteddy2@gmail.com>
2019-11-29 10:56:31 +0530

    done with test cases

--- bin/main.dart
+++ bin/main.dart
@@ -9,5 +9,94 @@
 
 
 // Stretch: Show all moves which lead to win, given a state.
+List<List<List<int>>> possibleMoves(List<List<int>> input){
+  List<List<List<int>>> result = [];
+
+  for(int i = 0; i< input.length; i++){
+    for(int j = 0; j< input.length; j++){
+      if(validMove(input, i, j)){
+        List<List<int>> copy=[];
+        for (List<int> columns in input){
+          copy.add(List.from(columns));
+        }
+        copy[j].insert(0, input[i][0]);
+        copy[i].remove(input[i][0]);
+        result.add(copy);
+      }
+    }
+  }
+  //print('after func $input = input  || $copy = copy');
+  return result;
+}
+
+
+List<List<int>> winInOneMove(List<List<int>> currentInput){
+  for(List<List<int>> move in possibleMoves(currentInput)){
+    if(move[0].isEmpty && move[1].isEmpty){
+      return move;
+    }
+  }
+  return null;
+}
+
+
+List<List<int>> winInTwoMove(List<List<int>> currentInput){
+
+  for(List<List<int>> move in (possibleMoves(currentInput))){
+    print(move);
+    if(winInOneMove(move) != null){
+      return move;
+    }
+  }
+  return null;
+}
+
+
+List<List<int>> winInThreeMove(List<List<int>> currentInput){
+
+  for(List<List<int>> move in (possibleMoves(currentInput))){
+    if(winInTwoMove(move) != null){
+      return move;
+    }
+  }
+  return null;
+}
+
+
+List<List<int>> winInNMoves(List<List<int>> currentInput, int n){
+  //print(currentInput);
+//  if(n == 1){
+//  }
+
+  List<List<List<int>>> listOfMoves = possibleMoves(currentInput);
+
+  for(List moves in listOfMoves){
+    if (n != 1) {
+      if(winInNMoves(moves, n - 1) !=null){
+        return moves;
+      };
+    }
+    else{
+      return(winInOneMove(currentInput));
+
+    }
+  }
+  return null;
+}
+
+void printMoves(List<List<int>> currentInput, int n){
+  for(int i = 0; i < n; n--){
+   print(winInNMoves(currentInput, n));
+   currentInput = winInNMoves(currentInput, n);
+  }}
+
+bool validMove(List<List<int>> input, int from, int to){
+  if(input[from].isEmpty) return false;
+  //print('to = $to & from = $from');
+  return(input[to].isEmpty || input[to].first > input[from].first);
+}
+
+
 main() {
+  printMoves([[1,2,3,4],[],[]], 15);
 }
--- test/Day_41_Morning_Challenge_test.dart
+++ test/Day_41_Morning_Challenge_test.dart
@@ -1,6 +1,14 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
   test('Test Case 1', () {
+//    expect(winInTwoMove([[1],[2],[3,4]]), [[1], [2], [3, 4]]);
+//    expect(winInOneMove([[1],[2],[3,4]]), null);
+//    expect(winInOneMove([[1],[],[2,3,4]]), [[1], [], [2, 3, 4]]);
+//    expect(winInTwoMove([[1],[],[2,3,4]]), null);
+    expect(winInNMoves([[1,2,3,4],[],[]], 15), [[2,3,4],[1],[]]);
+    expect(winInNMoves([[2,3,4],[1],[]], 14), [[3,4],[1],[2]]);
+    //expect(winInNMoves([[1,2,3,4,5,6],[],[]], 63), [[2,3,4,5,6],[1],[]]);
   });
 }
